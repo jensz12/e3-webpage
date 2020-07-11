c933391224834dd87e0b9e67b66c729f09aa4070
Jens Møller <jens@jensz12.com>
2020-07-11 17:17:05 +0200

    Update e3.php

--- e3.php
+++ e3.php
@@ -4,14 +4,14 @@
 <meta charset="utf-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<title>E3 2019 - Jens Møller</title>
+<title>E3 2021 - Jens Møller</title>
 <link rel="icon" href="https://design.jensz12.com/images/e3.png">
 <meta name="description" content="Electronic Entertainment Expo, E3 eller E³ er en årlig underholdningsmesse primært tiltænkt præsentation af fremtidige udgivelser indenfor computerspil og konsolspil">
 <meta name="theme-color" content="#A81010">
 <meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:site" content="@jensz12">
 <meta name="twitter:creator" content="@jensz12">
-<meta name="twitter:title" content="E3 2019 - Jens Møller">
+<meta name="twitter:title" content="E3 2021 - Jens Møller">
 <meta name="twitter:description" content="Electronic Entertainment Expo, E3 eller E³ er en årlig underholdningsmesse primært tiltænkt præsentation af fremtidige udgivelser indenfor computerspil og konsolspil">
 <meta name="twitter:image:src" content="https://design.jensz12.com/images/e3-2019.jpg">>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" crossorigin="anonymous">
@@ -49,7 +49,7 @@ h1 {
 	<div class="row">
 		<article class="col-lg-12 col-md-12">
 			<div class="jumbotron">
-				<h1> E3 starter om:</h1>
+				<h1> E3 2021 starter om:</h1>
 				<h1 id="countdown"></h1>
 			</div>
 			<div class="jumbotron">
@@ -65,43 +65,43 @@ h1 {
 						<tbody>
 							<tr>
 								<td><a href="#">EA</a></td>
-								<td>Er her ikke i år</td>
+								<td>TBA</td>
 								<td>TBA</td>
 							</tr>
 							<tr>
 								<td><a href="#">Microsoft</a></td>
-								<td>Søndag d. 9 juni</td>
-								<td>22:00</td>
+								<td>TBA</td>
+								<td>TBA</td>
 							</tr>
 							<tr>
 								<td><a href="#">Bethesda</a></td>
-								<td>Mandag d. 10 juni</td>
-								<td>02:30</td>
+								<td>TBA</td>
+								<td>TBA</td>
 							</tr>
 							<tr>
 								<td><a href="#">PC Gamer Show</a></td>
-								<td>Mandag d. 10 juni</td>
-								<td>19:00</td>
+								<td>TBA</td>
+								<td>TBA</td>
 							</tr>
 							<tr>
 								<td><a href="#">Ubisoft</a></td>
-								<td>Mandag d. 10 juni</td>
-								<td>22:00</td>
+								<td>TBA</td>
+								<td>TBA</td>
 							</tr>
 							<tr>
 								<td><a href="#">Square Enix</a></td>
-								<td>Tirsdag d. 11 juni</td>
-								<td>03:00</td>
+								<td>TBA</td>
+								<td>TBA</td>
 							</tr>
 							<tr>
 								<td><a href="#">Sony</a></td>
-								<td>Er her ikke i år</td>
+								<td>TBA</td>
 								<td>TBA</td>
 							</tr>
 							<tr>
 								<td><a href="#">Nintendo</a></td>
-								<td>Tirsdag d. 11 juni</td>
-								<td>18:00</td>
+								<td>TBA</td>
+								<td>TBA</td>
 							</tr>
 						</tbody>
 					</table>
@@ -127,7 +127,7 @@ h1 {
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"crossorigin="anonymous"></script>
 <script>
-	var countDownDate = new Date("Jun 9, 2019 22:00:00").getTime();
+	var countDownDate = new Date("Jun 15, 2021 22:00:00").getTime();
 	var x = setInterval(function() {
     var now = new Date().getTime();
     var distance = countDownDate - now;
@@ -136,7 +136,7 @@ h1 {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s "; 
+    + minutes + "m " + seconds + "s ";
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("countdown").innerHTML = "E3 er i gang";
